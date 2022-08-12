66ea1a55791532fb71d14f8133f61b221122b09b
cm2435 <cm2435@batch.ac.uk>
2022-08-12 08:30:54 +0100

    refactored code inferface and wrote basic test

---
+++ tests/unit/test_unixcoder.py
@@ -0,0 +1,19 @@
+import code
+import sys
+from pathlib import Path
+
+sys.path.insert(
+    0,
+    str(
+        Path(__file__).parents[2]
+        / "CodeClarity"
+        / "src"
+    ),
+)
+from unixcoder import UniXCoderEmbedder
+
+def test_embedding():
+    embedder = UniXCoderEmbedder(base_model = "microsoft/unixcoder-base")
+    embedder.encode(code_batch = "foo")
+
+    return None
